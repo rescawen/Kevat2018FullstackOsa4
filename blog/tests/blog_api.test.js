2296a4ce2829e3775e81b6e1706429d33dc9c405
rescawen <resca.wen@gmail.com>
2018-02-22 17:31:41 +0200

    progress

--- blog/tests/blog_api.test.js
+++ blog/tests/blog_api.test.js
@@ -9,6 +9,20 @@ test('notes are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
+test('there are two blogs', async () => {
+  const response = await api
+    .get('/api/blogs')
+
+  expect(response.body.length).toBe(2)
+})
+
+test('the first note is about HTTP methods', async () => {
+  const response = await api
+    .get('/api/blogs')
+
+  expect(response.body[0].title).toBe('String')
+})
+
 afterAll(() => {
   server.close()
 })
