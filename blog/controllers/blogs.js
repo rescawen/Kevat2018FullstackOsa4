5e91360f8ec9613a876f6519e5a40d4485c22e97
rescawen <resca.wen@gmail.com>
2018-02-25 18:17:03 +0200

    4.19 done?

--- blog/controllers/blogs.js
+++ blog/controllers/blogs.js
@@ -1,6 +1,8 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -20,9 +22,25 @@ blogsRouter.delete('/:id', async (request, response) => {
   }
 })
 
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
+
 blogsRouter.post('/', async (request, response) => {
+  const body = request.body
+
   try {
-    const body = request.body
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
 
     if (body.title === undefined) {
       return response.status(400).json({ error: 'title missing' })
